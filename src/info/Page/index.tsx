ec4fe9942ae5095d6359b2341f270eade0930402
AlkenD <desteralken@gmail.com>
2022-07-16 21:33:10 +0530

    remove: root example directory

--- src/info/Page/index.tsx
+++ src/info/Page/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import IntroBanner from '../../assets/banners/intro-banner.svg';
 
-const Page = ({ title }: any) => {
+const Page = ({ title, banner }: any) => {
   return (
     <div style={{width: '100%'}}>
       <h1>{title}</h1>
-      <img style={{width: '100%'}} src={IntroBanner} />
+      <img style={{width: '100%'}} src={banner} />
     </div>
   );
 };
